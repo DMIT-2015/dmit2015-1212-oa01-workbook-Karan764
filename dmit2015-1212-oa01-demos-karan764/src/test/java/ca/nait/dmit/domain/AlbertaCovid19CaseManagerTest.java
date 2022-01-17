986f8fc22907207f90214a43904889aa013a0910
Karan Bhagwani <bhag0012@gmail.com>
2022-01-17 11:52:16 -0700

    Updated test script

--- dmit2015-1212-oa01-demos-karan764/src/test/java/ca/nait/dmit/domain/AlbertaCovid19CaseManagerTest.java
+++ dmit2015-1212-oa01-demos-karan764/src/test/java/ca/nait/dmit/domain/AlbertaCovid19CaseManagerTest.java
@@ -5,7 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class AlbertaCovid19CaseManagerTest {
     @Test
     void getAlbertaCovid19CaseList() throws IOException {
-        AlbertaCovid19CaseManager caseManager = new AlbertaCovid19CaseManager();
+//        AlbertaCovid19CaseManager caseManager = new AlbertaCovid19CaseManager();
+        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
+        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+    }
+    @Test
+    void getAlbertaCovid19CaseList2() throws IOException {
+        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
+        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+    }
+    @Test
+    void getAlbertaCovid19CaseList3() throws IOException {
+        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
+        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+    }
+    @Test
+    void getAlbertaCovid19CaseList4() throws IOException {
+        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
+        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+    }
+    @Test
+    void getAlbertaCovid19CaseList5() throws IOException {
+        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
         assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
     }
 }
