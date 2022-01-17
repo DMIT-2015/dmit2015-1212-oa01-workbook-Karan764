890674ec33739a3241162a4be4852c3246fc39d2
Karan Bhagwani <bhag0012@gmail.com>
2022-01-17 11:50:04 -0700

    insert data from csv to a list, into to stream API

--- dmit2015-1212-oa01-demos-karan764/src/main/java/ca/nait/dmit/domain/AlbertaCovid19SummaryData.java
+++ dmit2015-1212-oa01-demos-karan764/src/main/java/ca/nait/dmit/domain/AlbertaCovid19SummaryData.java
@@ -1,2 +1,23 @@
-package ca.nait.dmit.domain;public class AlbertaCovid19SummaryData {
+package ca.nait.dmit.domain;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+public class AlbertaCovid19SummaryData {
+    // Define data field for each column in the CSV File
+    private int id;
+    private LocalDate dateReported;
+    private int numberOfLabTests;
+    private int cumulativeNumberOfLabTests;
+    private int numberOfCases;
+    private int cumulativeNumberOfCases;
+    private int activeCases;
+    private int currentlyHospitalized;
+    private int currentlyInICU;
+    private int cumulativeNumberOfDeaths;
+    private int numberOfDeaths;
+    private int numberOfVariantsOfConcern;
+    private double percentPositivity;
+
 }
---
+++ dmit2015-1212-oa01-demos-karan764/src/test/java/ca/nait/dmit/domain/AlbertaCovid19CaseManagerTest.java
@@ -0,0 +1,11 @@
+package ca.nait.dmit.domain;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
+class AlbertaCovid19CaseManagerTest {
+    @Test
+    void getAlbertaCovid19CaseList() throws IOException {
+        AlbertaCovid19CaseManager caseManager = new AlbertaCovid19CaseManager();
+        assertEquals(436495, caseManager.getAlbertaCovid19CaseList().size());
+    }
+}
