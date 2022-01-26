ec9550f76a9a670e25102a027eaea9e5587bd3a4
Karan Bhagwani <bhag0012@gmail.com>
2022-01-26 11:43:35 -0700

    completed jpa demo week 4

--- dmit2015-1212-oa01-jpa-demo/src/main/java/common/config/ApplicationConfig.java
+++ dmit2015-1212-oa01-jpa-demo/src/main/java/common/config/ApplicationConfig.java
@@ -7,6 +7,7 @@ import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.annotation.sql.DataSourceDefinitions;
 
 @DataSourceDefinitions({
+		/*
 	@DataSourceDefinition(
 		name="java:app/datasources/h2databaseDS",
 		className="org.h2.jdbcx.JdbcDataSource",
@@ -14,7 +15,14 @@ import jakarta.annotation.sql.DataSourceDefinitions;
 //        url="jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
 		user="sa",
 		password="sa"),
-
+*/
+		@DataSourceDefinition(
+				name="java:app/datasources/hsqldatabaseDS",
+				className="org.hsqldb.jdbc.JDBCDataSource",
+	//	url="jdbc:hsqldb:file:~/jdk/databases/dmit2015-demos-hsqldb;shutdown=true", // see the file generated
+				url="jdbc:hsqldb:mem:dmit2015hsqldb",
+				user="user2015",
+				password="Password2015"),
 //	@DataSourceDefinition(
 //		name="java:app/datasources/hsqldatabaseDS",
 //		className="org.hsqldb.jdbc.JDBCDataSource",
